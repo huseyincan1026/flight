fd2b8d954ca83e8bc8c01fbb2aa255a4c349d034
Hüseyincan Öçal <73823956+huseyincan1026@users.noreply.github.com>
2024-11-02 16:11:50 +0300

    Update flight.py

--- flight.py
+++ flight.py
@@ -1,4 +1,4 @@
-from selenium import webdriver
+=from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -35,8 +35,8 @@ def run():
 
 
 
-    
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service = service, options=options)
     driver.get(url)
     
     try:
